7775f8444c5ecc28069d67fac0f3b093d700c66c
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-04 15:32:04 +0900

    refactor: フォーマットと追加した関数型の適用

--- lib/parse/parse-path.ts
+++ lib/parse/parse-path.ts
@@ -1,6 +1,5 @@
-import { NotionUrl } from "../notion-url/types";
+import { NotionUrl, parsePath as parsePathFunction } from "../notion-url/types";
 import { validate } from "../validation/validation";
-
 
 /**
  * URLを解析し、NotionUrlオブジェクトを返します。
@@ -8,7 +7,7 @@ import { validate } from "../validation/validation";
  * @returns 解析されたNotionUrlオブジェクト。
  * @throws URLが無効な場合はエラーが throw されます。
  */
-export const parsePath: (rawUrl: string) => NotionUrl = (rawUrl: string) => {
+export const parsePath: parsePathFunction = (rawUrl: string) => {
 	if (!validate(rawUrl)) {
 		throw new Error("Invalid URL");
 	}
@@ -31,10 +30,10 @@ export const parsePath: (rawUrl: string) => NotionUrl = (rawUrl: string) => {
  * @returns ワークスペースとページIDが含まれるオブジェクト（利用可能な場合）。
  */
 const separatePathName = (path: string) => {
-    const pathArray = path.split("/").filter((item) => item !== "");
-    if (pathArray.length >= 2) {
-        return { workspace: pathArray[0], pageId: pathArray[1] };
-    }
+	const pathArray = path.split("/").filter((item) => item !== "");
+	if (pathArray.length >= 2) {
+		return { workspace: pathArray[0], pageId: pathArray[1] };
+	}
 
-    return { pageId: pathArray[0] };
+	return { pageId: pathArray[0] };
 };
