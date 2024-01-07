336c73dc080ceb82670decdee28a77e79a590f0f
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-07 17:04:10 +0900

    feat: 公開する型情報を複数のparse関数に対応する形に変更

--- lib/notion-url/types.ts
+++ lib/notion-url/types.ts
@@ -3,7 +3,17 @@ export type NotionUrl = {
 	rawPageId: string;
 	pageId: string;
 	workspaceId?: string;
+	rawDatabasePageId?: string;
+	databasePageId?: string;
+	viewId?: string;
 };
 
-export type parsePath = (rawUrl: string) => NotionUrl;
+export type ParsedFromParam = Pick<NotionUrl, "raw" | "viewId" | "databasePageId">;
+export type ParsedFromPath = Pick<
+	NotionUrl,
+	"raw" | "rawPageId" | "pageId" | "workspaceId"
+>;
+
+export type parseParam = (rawUrl: string) => ParsedFromParam;
+export type parsePath = (rawUrl: string) => ParsedFromPath;
 export type validate = (rawUrl: string) => boolean;
