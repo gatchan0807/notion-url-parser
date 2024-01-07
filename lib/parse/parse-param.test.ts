093e60019646fc7f7357e2bab9f3e0a70a41160e
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-07 22:21:15 +0900

    test: 異常系テストの追加

--- lib/parse/parse-param.test.ts
+++ lib/parse/parse-param.test.ts
@@ -4,18 +4,6 @@ import { parseParam } from "./parse-param";
 describe("parseParam", () => {
     test.each([
         {
-            url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
-            expected: {
-                raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
-            },
-        },
-        {
-            url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-            expected: {
-                raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-            },
-        },
-        {
             url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?v=abcdefghijklnmopqrstuv1234567890",
             expected: {
                 raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?v=abcdefghijklnmopqrstuv1234567890",
@@ -36,18 +24,18 @@ describe("parseParam", () => {
                 isDatabasePage: true,
             },
         },
-    ])("[正常系] $url の場合パース成功する", ({ url, expected }) => {
+    ])("[正常系] $url の場合、パース成功する", ({ url, expected }) => {
         expect(parseParam(url)).toStrictEqual(expected);
     });
 
     test.each([
         {
-            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）",
             url: "https://notion.so",
             expectedException: "Invalid URL",
         },
         {
-            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）",
             url: "https://workspace.notion.so",
             expectedException: "Invalid URL",
         },
@@ -72,9 +60,56 @@ describe("parseParam", () => {
             expectedException: "Invalid URL",
         },
     ])(
-        "[異常系] $caseTitle( $url )の場合パース失敗する",
+        "[異常系] $caseTitle( $url )の場合、パース失敗する",
         ({ url, expectedException }) => {
             expect(() => parseParam(url)).toThrowError(expectedException);
         }
     );
+
+    test.each([
+        {
+            caseTitle: "URLにvのパラメーターが含まれていない",
+            url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
+            expected: {
+                raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
+            },
+        },
+        {
+            caseTitle: "URLにvのパラメーターが含まれていない",
+            url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+            expected: {
+                raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+            },
+        },
+        {
+            caseTitle: "NotionのParamsとして想定されないパラメータが含まれている",
+            url: "https://notion.so/1234567890abcdefghijklnmopqrstuv?x=abcdefghijklnmopqrstuv1234567890",
+            expected: {
+                raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv?x=abcdefghijklnmopqrstuv1234567890",
+            },
+        },
+        {
+            caseTitle: "NotionのParamsとして想定されないパラメータが含まれている",
+            url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?x=abcdefghijklnmopqrstuv1234567890",
+            expected: {
+                raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?x=abcdefghijklnmopqrstuv1234567890",
+            },
+        },
+        {
+            caseTitle: "NotionのParamsとして想定されないパラメータが含まれている",
+            url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?x",
+            expected: {
+                raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?x",
+            },
+        },
+        {
+            caseTitle: "vパラメータの中身が含まれていない",
+            url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?v",
+            expected: {
+                raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv?v",
+            },
+        },
+    ])("[異常系] $caseTitle ( $url )場合、そのままURLを返す形でパース成功する", ({ url, expected }) => {
+        expect(parseParam(url)).toStrictEqual(expected);
+    });
 });
