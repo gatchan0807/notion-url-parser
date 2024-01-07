ef01ddb6ad6cdaa9bcd44e89e455310cdca3413e
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-07 21:54:42 +0900

    test: パース出来ないURLが入った場合の異常系テストを追加

--- lib/parse/parse-param.test.ts
+++ lib/parse/parse-param.test.ts
@@ -39,4 +39,42 @@ describe("parseParam", () => {
     ])("[正常系] $url の場合パース成功する", ({ url, expected }) => {
         expect(parseParam(url)).toStrictEqual(expected);
     });
+
+    test.each([
+        {
+            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+            url: "https://notion.so",
+            expectedException: "Invalid URL",
+        },
+        {
+            caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+            url: "https://workspace.notion.so",
+            expectedException: "Invalid URL",
+        },
+        {
+            caseTitle: "Notion以外のURL",
+            url: "https://google.com",
+            expectedException: "Invalid URL",
+        },
+        {
+            caseTitle: "Notion以外のURL（Param付き）",
+            url: "https://google.com?query=abcxyz",
+            expectedException: "Invalid URL",
+        },
+        {
+            caseTitle: "URLとして成立しない文字列",
+            url: "https://",
+            expectedException: "Invalid URL",
+        },
+        {
+            caseTitle: "Notion Page IDだけの場合",
+            url: "1234567890abcdefghijklnmopqrstuv",
+            expectedException: "Invalid URL",
+        },
+    ])(
+        "[異常系] $caseTitle( $url )の場合パース失敗する",
+        ({ url, expectedException }) => {
+            expect(() => parseParam(url)).toThrowError(expectedException);
+        }
+    );
 });
