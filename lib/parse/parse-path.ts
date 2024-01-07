8fdff22ff25cd842c5e3e4928b5260fa71b4b340
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-07 20:57:48 +0900

    refactor: 切り出した関数に差し替え

--- lib/parse/parse-path.ts
+++ lib/parse/parse-path.ts
@@ -1,4 +1,5 @@
 import { parsePath as parsePathFunction } from "../notion-url/types";
+import { separatePageId, separatePathName } from "../common/domain";
 import { validate } from "../validation/validation";
 
 /**
@@ -13,6 +14,7 @@ export const parsePath: parsePathFunction = (rawUrl: string) => {
 	}
 
 	const url = new URL(rawUrl);
+
 	const path = separatePathName(url.pathname);
 	const pageId = separatePageId(path.rawPageId);
 
@@ -20,34 +22,6 @@ export const parsePath: parsePathFunction = (rawUrl: string) => {
 		raw: rawUrl,
 		rawPageId: path.rawPageId,
 		pageId: pageId ?? "",
-		workspaceId: path.workspaceId ?? "",
+		workspaceId: path.rawWorkspaceId ?? "",
 	};
-};
-
-/**
- * パス名をワークスペースとページIDに分割します。
- * @param path - 分割するパス名。
- * @returns ワークスペースとページIDが含まれるオブジェクト（利用可能な場合）。
- */
-const separatePathName = (path: string) => {
-	const pathArray = path.split("/").filter((item) => item !== "");
-	if (pathArray.length >= 2) {
-		return { workspaceId: pathArray[0], rawPageId: pathArray[1] };
-	}
-
-	return { rawPageId: pathArray[0] };
-};
-
-/**
- * ページIDを生のページIDから分離します。
- *
- * @param rawPageId - 生のページID。
- * @returns 分離されたページID。
- */
-const separatePageId = (rawPageId: string) => {
-	const pageId = rawPageId.match(/[a-z0-9]{32}$/);
-	if (!pageId || pageId[0] === "") {
-		return null;
-	}
-	return pageId[0];
 };
