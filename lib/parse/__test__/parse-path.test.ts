ab0d94502c0603c7ba379bdde530c21b9fa56bcc
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-01-07 22:38:29 +0900

    refactor: テストのdescribeを統一し、一覧性を上げる

--- lib/parse/__test__/parse-path.test.ts
+++ lib/parse/__test__/parse-path.test.ts
@@ -2,169 +2,175 @@ import { describe, expect, test } from "vitest";
 import { parsePath } from "../parse-path";
 
 describe("parsePath", () => {
-	test.each([
-		{
-			url: "https://notion.so/1234567890abcdefghijklnmopqrstuv",
-			expected: {
-				raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv",
-				rawPageId: "1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+	describe("正常系", () => {
+		test.each([
+			{
+				url: "https://notion.so/1234567890abcdefghijklnmopqrstuv",
+				expected: {
+					raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv",
+					rawPageId: "1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-			expected: {
-				raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-				rawPageId: "1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+				expected: {
+					raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+					rawPageId: "1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-			expected: {
-				raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+				expected: {
+					raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-			expected: {
-				raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-				rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+				expected: {
+					raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+					rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
-			expected: {
-				raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
-				rawPageId: "1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "workspace",
+			{
+				url: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
+				expected: {
+					raw: "https://notion.so/workspace/1234567890abcdefghijklnmopqrstuv",
+					rawPageId: "1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "workspace",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-			expected: {
-				raw: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "workspace",
+			{
+				url: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+				expected: {
+					raw: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "workspace",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-			expected: {
-				raw: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
-				rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "workspace",
+			{
+				url: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+				expected: {
+					raw: "https://notion.so/workspace/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv?query=abcxyz",
+					rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "workspace",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
-			expected: {
-				raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
-				rawPageId: "1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
+				expected: {
+					raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
+					rawPageId: "1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890?query=abcxyz",
-			expected: {
-				raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890?query=abcxyz",
-				rawPageId: "1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890?query=abcxyz",
+				expected: {
+					raw: "https://notion.so/1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890?query=abcxyz",
+					rawPageId: "1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
-			expected: {
-				raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
-				rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
-				pageId: "1234567890abcdefghijklnmopqrstuv",
-				workspaceId: "",
+			{
+				url: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
+				expected: {
+					raw: "https://notion.so/alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv#abcdefghijklnmopqrstuv1234567890",
+					rawPageId: "alphabet-in-page-title-1234567890abcdefghijklnmopqrstuv",
+					pageId: "1234567890abcdefghijklnmopqrstuv",
+					workspaceId: "",
+				},
 			},
-		},
-	])("[正常系] $url の場合パース成功する", ({ url, expected }) => {
-		expect(parsePath(url)).toStrictEqual(expected);
+		])("$url の場合パース成功する", ({ url, expected }) => {
+			expect(parsePath(url)).toStrictEqual(expected);
+		});
 	});
 
-	test.each([
-		{
-			caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
-			url: "https://notion.so",
-			expectedException: "Invalid URL",
-		},
-		{
-			caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
-			url: "https://workspace.notion.so",
-			expectedException: "Invalid URL",
-		},
-		{
-			caseTitle: "Notion以外のURL",
-			url: "https://google.com",
-			expectedException: "Invalid URL",
-		},
-		{
-			caseTitle: "URLとして成立しない文字列",
-			url: "https://",
-			expectedException: "Invalid URL",
-		},
-		{
-			caseTitle: "Notion Page IDだけの場合",
-			url: "1234567890abcdefghijklnmopqrstuv",
-			expectedException: "Invalid URL",
-		},
-	])(
-		"[異常系] $caseTitle( $url )の場合パース失敗する",
-		({ url, expectedException }) => {
-			expect(() => parsePath(url)).toThrowError(expectedException);
-		}
-	);
+	describe("異常系（パース失敗）", () => {
+		test.each([
+			{
+				caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+				url: "https://notion.so",
+				expectedException: "Invalid URL",
+			},
+			{
+				caseTitle: "パスがルートのNotionのURL（サブドメイン無し）の場合",
+				url: "https://workspace.notion.so",
+				expectedException: "Invalid URL",
+			},
+			{
+				caseTitle: "Notion以外のURL",
+				url: "https://google.com",
+				expectedException: "Invalid URL",
+			},
+			{
+				caseTitle: "URLとして成立しない文字列",
+				url: "https://",
+				expectedException: "Invalid URL",
+			},
+			{
+				caseTitle: "Notion Page IDだけの場合",
+				url: "1234567890abcdefghijklnmopqrstuv",
+				expectedException: "Invalid URL",
+			},
+		])(
+			"$caseTitle( $url )の場合パース失敗する",
+			({ url, expectedException }) => {
+				expect(() => parsePath(url)).toThrowError(expectedException);
+			}
+		);
+	});
 
-	test.each([
-		{
-			caseTitle:
-				"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
-			url: "https://notion.so/1234567890abc",
-			expected: {
-				raw: "https://notion.so/1234567890abc",
-				rawPageId: "1234567890abc",
-				pageId: "",
-				workspaceId: "",
+	describe("異常系（フェイルセーフ）", () => {
+		test.each([
+			{
+				caseTitle:
+					"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
+				url: "https://notion.so/1234567890abc",
+				expected: {
+					raw: "https://notion.so/1234567890abc",
+					rawPageId: "1234567890abc",
+					pageId: "",
+					workspaceId: "",
+				},
 			},
-		},
-		{
-			caseTitle:
-				"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
-			url: "https://notion.so/workspace/1234567890abc",
-			expected: {
-				raw: "https://notion.so/workspace/1234567890abc",
-				rawPageId: "1234567890abc",
-				pageId: "",
-				workspaceId: "workspace",
+			{
+				caseTitle:
+					"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
+				url: "https://notion.so/workspace/1234567890abc",
+				expected: {
+					raw: "https://notion.so/workspace/1234567890abc",
+					rawPageId: "1234567890abc",
+					pageId: "",
+					workspaceId: "workspace",
+				},
 			},
-		},
-		{
-			caseTitle:
-				"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
-			url: "https://notion.so/alphabet-in-page-title-1234567890abc",
-			expected: {
-				raw: "https://notion.so/alphabet-in-page-title-1234567890abc",
-				rawPageId: "alphabet-in-page-title-1234567890abc",
-				pageId: "",
-				workspaceId: "",
+			{
+				caseTitle:
+					"Notion URL (ID部分) が正しくない形の場合、Page IDが空文字列になる",
+				url: "https://notion.so/alphabet-in-page-title-1234567890abc",
+				expected: {
+					raw: "https://notion.so/alphabet-in-page-title-1234567890abc",
+					rawPageId: "alphabet-in-page-title-1234567890abc",
+					pageId: "",
+					workspaceId: "",
+				},
 			},
-		},
-	])("[異常系] $caseTitle( $url )", ({ url, expected }) => {
-		expect(parsePath(url)).toStrictEqual(expected);
+		])("$caseTitle( $url )", ({ url, expected }) => {
+			expect(parsePath(url)).toStrictEqual(expected);
+		});
 	});
 });
